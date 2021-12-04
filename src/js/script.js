fb99d22aebcd3631f9759b99e5748f778c1930b5
Serhii Sapiannyk <serhii09@icloud.com>
2021-12-04 13:15:40 +0100

    not work

--- src/js/script.js
+++ src/js/script.js
@@ -64,6 +64,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
       // console.log('thisProduct:', thisProduct);
     }
@@ -92,6 +93,7 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
 
     initAccordion(){
@@ -198,8 +200,75 @@
       thisProduct.priceElem.innerHTML = price;
       
     }
+    initAmountWidget(){
+      const thisProduct = this;
+
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+    }
   }
- 
+
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+      // console.log(thisWidget);
+     
+      thisWidget.getElements(element);
+      
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
+      console.log('AmoutWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+    }
+    
+    getElements(element){
+      const thisWidget = this;
+      
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+      
+    }
+    setValue(value){
+      const thisWidget = this;
+      
+      const newValue = parseInt(value);
+      // TODO: ADD VALIDATION
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value; 
+
+      if(thisWidget.value !== newValue){
+        thisWidget.value = newValue;
+      }
+      if(thisWidget.value !== newValue && !isNaN(newValue)){
+        thisWidget.value = newValue;
+      }
+      // if(thisWidget.value !== newValue >= settings.amountWidget.defaultMin){
+      //   thisWidget.value = newValue;
+      // }
+      
+    }
+    
+    initActions(){
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);
+        
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value -1 );
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value +1 );
+      });
+    }
+  }
+  
   const app = {
     initData: function(){
       const thisApp = this;
